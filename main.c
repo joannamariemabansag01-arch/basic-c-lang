d935b0e39c1a25d8fced334492f16b78f3577157
Kiel Astillasa <kielelijahastillasa@gmail.com>
2025-11-14 14:01:34 +0800

    created basic syntax file for c project

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age = 20; //format specifier: %d
+    int chsd_asdh = 20;
+    int x = 22;
+    char initial_name = 'h';
+    float ave_grade = 96.5;
+    float y = 49.54656;
+
+    printf ("Age: %d \n", age);
+    printf ("Initial letter of the name: %c \n", initial_name);
+    printf ("Average Grade: %f", ave_grade);
+    printf ("Float with Specific no. of decimal %.2f");
+    return 0;
+}
